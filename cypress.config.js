e3a1fb55ff31793c11cb79d1759eb97130d530a5
BriannaJRowe <kaibrianna01@gmail.com>
2022-12-07 23:15:43 -0500

    config update

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,10 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+
+    //base url and spec pattern
+    baseUrl: 'https://ui-automation-camp.vercel.app/', 
+    specPattern: 'cypress/e2e/test/**/*.cy.{js,jsx,ts,tsx}',
+
   },
 });
